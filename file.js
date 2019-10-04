c8d32652ec59b216870eb3dc1f03cde5108889ac
Ali-hd <ali.hamidaddin1@gmail.com>
2019-10-04 14:14:10 +0300

    Fixed if user starts from Union

--- file.js
+++ file.js
@@ -9,7 +9,7 @@ let sub = {
     6 : ["Grand Central","33rd","28th","23rd","Union Square","Astor Place"]
   }
   for(let i = (sub[atline].indexOf(atstop))+1;i<=(sub[atline].length);i++){
-
+    
     if(sub[atline][i]=="Times square"){
       i = i + 1
     }
@@ -18,13 +18,13 @@ let sub = {
 
       sen = sen + sub[atline][i]
       sen = sen + "."
-
+      
       break;
     }
-
+    
     sen = sen + sub[atline][i]
     sen = sen + ", "
-
+    
     if(sub[atline][i]==tostop && atline==toline){
       sameline = sen ;
       break;
@@ -32,78 +32,82 @@ let sub = {
     }
 
    if(sen.indexOf("Union Square")==-1){
-
+     
       sen=""
    }
 
   if(sen==""){
-
+    
     for(let i = ((sub[atline].indexOf(atstop))-1);i>=0;i--){
-
+      
     if(sub[atline][i]=="Union Square"){
       sen = sen + sub[atline][i]
       sen = sen + "."
-
+      
       break;
-    }
-
+    } 
+      
      sen = sen + sub[atline][i]
-     sen = sen + ", "
-
+     sen = sen + ", " 
+     
   }
 
   }
-
-  if(sen.indexOf('Union')>=0){
+  
+  if(sen.indexOf('Union')>=0){ 
     for(let i = sub[toline].indexOf("Union Square");i<sub[toline].length;i++){
-
+      
       if(tostop == "Union Square"){
         console.log("You have reached your destination")
         break;
       }else
       if(sub[toline][i]==tostop){
-
+        
         rtv = rtv + sub[toline][i]
         rtv = rtv + "."
         break;
       }
         if(sub[toline][i]!="Union Square")
-        rtv = rtv + sub[toline][i]
+        rtv = rtv + sub[toline][i]  
 
         if(sub[toline][i]!="Union Square")
         rtv = rtv + ", "
-
-
+        
+        
       }
   }
-
+     
       if(rtv.indexOf(tostop)==-1){
         rtv = "";
         for(let i = sub[toline].indexOf("Union Square");i>=0;i--){
-
+          
           if(tostop == "Union Square"){
               console.log("You have reached your destination")
             break;
           }else
         if(sub[toline][i]==tostop){
-
+                
                 rtv = rtv + sub[toline][i]
                 rtv = rtv + "."
                 break;
               }
 
         if(sub[toline][i]!="Union Square")
-        rtv = rtv + sub[toline][i]
+        rtv = rtv + sub[toline][i]  
 
         if(sub[toline][i]!="Union Square")
         rtv = rtv + ", "
 
+        
+      }
+      }
 
-      }
-      }
+  if(atstop == "Union Square")
+  sen = " "
+
 let add1 = sen.split(",")
 let add2 = rtv.split(",")
-let sum = (add1.length + add2.length)
+let sum = (add1.length + add2.length) 
 if(sameline.length > 0){
   console.log("You must travel through the following stops on the " + atline + " line: "+sameline)
 }else{
@@ -120,4 +124,4 @@ console.log( sum + " stops in total")
 }
 
 
-subway("N", "Times Square", "6", "33rd")
+subway("6", "Union Square", "6", "33rd")
