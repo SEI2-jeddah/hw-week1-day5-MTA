b1dd0baff439ee42b285e63c5b970d3ecc1702a8
Ali-hd <ali.hamidaddin1@gmail.com>
2019-10-04 16:21:30 +0300

    Fix if user at Union or goto before union + spelling in obj element.

--- file.js
+++ file.js
@@ -4,16 +4,12 @@ function subway(atline,atstop,toline,tostop){
   let sameline = 0;
 
 let sub = {
-    N : ["Times square","34th","28th","23rd","Union Square","8th"],
+    N : ["Times Square","34th","28th","23rd","Union Square","8th"],
     L : ["8th","6th","Union Square","3rd","1st"],
     6 : ["Grand Central","33rd","28th","23rd","Union Square","Astor Place"]
   }
   for(let i = (sub[atline].indexOf(atstop))+1;i<=(sub[atline].length);i++){
     
-    if(sub[atline][i]=="Times square"){
-      i = i + 1
-    }
-
     if(sub[atline][i]=="Union Square"){
 
       sen = sen + sub[atline][i]
@@ -58,7 +54,7 @@ let sub = {
     for(let i = sub[toline].indexOf("Union Square");i<sub[toline].length;i++){
       
       if(tostop == "Union Square"){
-        console.log("You have reached your destination")
+        //console.log("You have reached your destination")
         break;
       }else
       if(sub[toline][i]==tostop){
@@ -82,15 +78,38 @@ let sub = {
         for(let i = sub[toline].indexOf("Union Square");i>=0;i--){
           
           if(tostop == "Union Square"){
-              console.log("You have reached your destination")
+              //console.log("You have reached your destination")
             break;
           }else
-        if(sub[toline][i]==tostop){
-                
+        if(sub[toline][i]==tostop){   
                 rtv = rtv + sub[toline][i]
                 rtv = rtv + "."
                 break;
-              }
+        }
+
+        if(sub[toline][i]!="Union Square")
+        rtv = rtv + sub[toline][i]  
+
+        if(sub[toline][i]!="Union Square")
+        rtv = rtv + ", "
+
+        
+      }
+      }
+
+     if(rtv.indexOf(tostop)==-1){
+        rtv = "";
+        for(let i = sub[toline].indexOf("Union Square");i>=0;i++){
+          
+          if(tostop == "Union Square"){
+              //console.log("You have reached your destination")
+            break;
+          }else
+        if(sub[toline][i]==tostop){   
+                rtv = rtv + sub[toline][i]
+                rtv = rtv + "."
+                break;
+        }
 
         if(sub[toline][i]!="Union Square")
         rtv = rtv + sub[toline][i]  
@@ -105,23 +124,28 @@ let sub = {
   if(atstop == "Union Square")
   sen = " "
 
+if(sameline>0 || sameline.length>0){sameline = sameline.slice(0,-2)};
 let add1 = sen.split(",")
+if(atline == toline && (sameline>0 || sameline.length>0)){add1 = sameline.split(",")}
 let add2 = rtv.split(",")
-let sum = (add1.length + add2.length) 
-if(sameline.length > 0){
-  console.log("You must travel through the following stops on the " + atline + " line: "+sameline)
-}else{
-console.log("You must travel through the following stops on the " + atline + " line: "+sen)
+if(atline == toline && (sameline>0 || sameline.length>0)){add2 = ""}
+let sum = (add1.length + add2.length)
+if(atline == toline && (sameline>0 || sameline.length>0)){sum += 1}
+if(atline == toline && (sameline>0 || sameline.length>0) && (sameline.indexOf('Union')==-1)||(atstop=="Union Square")){sum-=1}
+if(sameline.length > 0 && atstop!="Union Square"){
+  console.log("You must travel through the following stops on the " + atline + " line: "+sameline+ ".")
+}else if(atstop!="Union Square"){
+console.log("You must travel through the following stops on the " + atline + " line: "+sen)}
 if(atline!=toline){
-  console.log("Change at Union Square.")
-}else{
+  console.log("Change at Union Square to line "+toline+".")
+}else if(tostop != "Union Square"){
   console.log("Stay on the same line")
 }
-console.log("You journey continues through the following stops: "+rtv)
+if(tostop != "Union Square"){console.log("Your journey continues through the following stops: "+rtv)}
 
-}
+
 console.log( sum + " stops in total")
 }
 
 
-subway("6", "Union Square", "6", "33rd")
+subway("L", "Union Square", "L", "6th")
