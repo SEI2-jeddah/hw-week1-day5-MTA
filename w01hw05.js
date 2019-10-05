9e7dd6b1de9d5d8a5f2ab1b7821fb26b83f3bc36
Doaa AlHajjaji <Doaa.mh.uni@gmail.com>
2019-10-05 13:02:18 +0300

    formatted the code

--- w01hw05.js
+++ w01hw05.js
@@ -4,91 +4,91 @@ var mTAPaths = {
     '6': ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
 }
 
-function planTrip(currPath, currLocation, destinationPath, destination){
-let hops = [] // to store the hops
-let hopsCounter = 0 // to count the hops
+function planTrip(currPath, currLocation, destinationPath, destination) {
+    let hops = [] // to store the hops
+    let hopsCounter = 0 // to count the hops
 
-let currIndex = mTAPaths[currPath].indexOf(currLocation) //stores current location index
-let stopsIndex = 0 //stores the destination or intersection point
+    let currIndex = mTAPaths[currPath].indexOf(currLocation) //stores current location index
+    let stopsIndex = 0 //stores the destination or intersection point
 
-//in case the destination path is the same as the current path and the destination is before the intersection point
-if(destinationPath==currPath){
-    stopsIndex = mTAPaths[currPath].indexOf(destination)
-}else{
-    stopsIndex = mTAPaths[currPath].indexOf("Union Square")
-}
-
-//start of the while loop to iterate through the trip
-while (currLocation != destination || destinationPath != currPath) {
-
-    hops.push(currPath) // stores the current path (for printing)
-
-    //checks if the the metro is heading to the front or the back to the union square
-    //the iterates through the path array and stores the hops in the hops array
-    if (currIndex > stopsIndex) {
-        for (let i = currIndex - 1; i >= stopsIndex; i--) {
-            hops.push(mTAPaths[currPath][i])
-            currLocation = mTAPaths[currPath][i]
-            hopsCounter++
-        }
-    } else{
-        for (let i = currIndex + 1; i <= stopsIndex; i++) {
-            hops.push(mTAPaths[currPath][i])
-            currLocation = mTAPaths[currPath][i]
-            hopsCounter++
-        }
+    //in case the destination path is the same as the current path and the destination is before the intersection point
+    if (destinationPath == currPath) {
+        stopsIndex = mTAPaths[currPath].indexOf(destination)
+    } else {
+        stopsIndex = mTAPaths[currPath].indexOf("Union Square")
     }
 
-    //check if the metro is at the union square then checks if there's a change in the path
-    //then change the stop index fron the union square to the final destination
-    if (currLocation == "Union Square") {
-        if (currPath == destinationPath) {
-            stopsIndex = mTAPaths[currPath].indexOf(destination)
-            currIndex = mTAPaths[currPath].indexOf("Union Square")
+    //start of the while loop to iterate through the trip
+    while (currLocation != destination || destinationPath != currPath) {
+
+        hops.push(currPath) // stores the current path (for printing)
+
+        //checks if the the metro is heading to the front or the back to the union square
+        //the iterates through the path array and stores the hops in the hops array
+        if (currIndex > stopsIndex) {
+            for (let i = currIndex - 1; i >= stopsIndex; i--) {
+                hops.push(mTAPaths[currPath][i])
+                currLocation = mTAPaths[currPath][i]
+                hopsCounter++
+            }
         } else {
-            hops.push("Change at Union Square.")
-            currPath = destinationPath
-            stopsIndex = mTAPaths[currPath].indexOf(destination)
-            currIndex = mTAPaths[currPath].indexOf("Union Square")
+            for (let i = currIndex + 1; i <= stopsIndex; i++) {
+                hops.push(mTAPaths[currPath][i])
+                currLocation = mTAPaths[currPath][i]
+                hopsCounter++
+            }
         }
-        //change the currLocation to be on the right path
-        currLocation = mTAPaths[currPath][currIndex]
-        
+
+        //check if the metro is at the union square then checks if there's a change in the path
+        //then change the stop index fron the union square to the final destination
+        if (currLocation == "Union Square") {
+            if (currPath == destinationPath) {
+                stopsIndex = mTAPaths[currPath].indexOf(destination)
+                currIndex = mTAPaths[currPath].indexOf("Union Square")
+            } else {
+                hops.push("Change at Union Square.")
+                currPath = destinationPath
+                stopsIndex = mTAPaths[currPath].indexOf(destination)
+                currIndex = mTAPaths[currPath].indexOf("Union Square")
+            }
+            //change the currLocation to be on the right path
+            currLocation = mTAPaths[currPath][currIndex]
+
+        }
     }
-}
 
 
-//printing the trip
-if(hops.includes("Change at Union Square.")){
-changeAtUSIndex = hops.indexOf("Change at Union Square.")
-let firstLinePath = " "
-let secondLinePath = " "
-for(let i=1;i<changeAtUSIndex-1;i++){
-    firstLinePath = firstLinePath + hops[i]+", "
-}
-firstLinePath = firstLinePath + hops[changeAtUSIndex-1]+"."
-for(let i=changeAtUSIndex+2;i<hops.length-1;i++){
-    secondLinePath = secondLinePath + hops[i]+", "
-}
-secondLinePath = secondLinePath +hops[hops.length-1]+"."
-console.log("You must travel through the following stops on the "+hops[0]+" line:"+firstLinePath)
-console.log(hops[changeAtUSIndex])
-console.log("Your journey continues through the following stops:"+secondLinePath)
-console.log(hopsCounter + " stops in total")
+    //printing the trip
+    if (hops.includes("Change at Union Square.")) {
+        changeAtUSIndex = hops.indexOf("Change at Union Square.")
+        let firstLinePath = " "
+        let secondLinePath = " "
+        for (let i = 1; i < changeAtUSIndex - 1; i++) {
+            firstLinePath = firstLinePath + hops[i] + ", "
+        }
+        firstLinePath = firstLinePath + hops[changeAtUSIndex - 1] + "."
+        for (let i = changeAtUSIndex + 2; i < hops.length - 1; i++) {
+            secondLinePath = secondLinePath + hops[i] + ", "
+        }
+        secondLinePath = secondLinePath + hops[hops.length - 1] + "."
+        console.log("You must travel through the following stops on the " + hops[0] + " line:" + firstLinePath)
+        console.log(hops[changeAtUSIndex])
+        console.log("Your journey continues through the following stops:" + secondLinePath)
+        console.log(hopsCounter + " stops in total")
 
-}else{
-    let line = " "
-    for(let i=1;i<hops.length-1;i++){
-    if(hops[i]==hops[0]){
-        continue
+    } else {
+        let line = " "
+        for (let i = 1; i < hops.length - 1; i++) {
+            if (hops[i] == hops[0]) {
+                continue
+            }
+            line = line + hops[i] + ", "
+        }
+        line = line + hops[hops.length - 1] + "."
+        console.log("You must travel through the following stops on the " + hops[0] + " line:" + line)
+        console.log(hopsCounter + " stops in total")
     }
-    line = line +hops[i]+", "
-}
-line = line +hops[hops.length-1]+"."
-console.log("You must travel through the following stops on the "+hops[0]+" line:"+line)
-    console.log(hopsCounter + " stops in total")
-}
 }// end of planTrip function
 
 
-planTrip("N","Times Square","6","33rd")
+planTrip("N", "Times Square", "6", "33rd")
