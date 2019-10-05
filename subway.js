09dacc310f4c5825228826c5f5420a36870d7272
iashjan <ashjanot@gmail.com>
2019-10-05 22:04:29 +0300

    subway completed

---
+++ subway.js
@@ -0,0 +1,20 @@
+let N = ["timesSquar", "34th", "28th", "23rd", "Union Square", "8th",];
+let L = ["8th", "6th", "us", "3rd", "1st"];
+let six = ['33rd', '28thStreet', '23th', 'Union Square', 'Astor Place'];
+
+function planTrip(startDest, startLine, endDest, endLine){
+if(startDest = startLine){
+    var N = startLine.indexOf ("timesSquar");
+}; for (i = 0; i < N.length; i++) // "You must travel through the following stops on the N line: 34th, 28th, 23rd, Union Square."
+};
+ if else(N = startLine.indexOf("Union Square"){
+     for( i <= Union Square) // "Change at Union Square."
+ };
+ 
+ else (endDest = endLine) {
+     var six = endLine.includes('Union Square');
+     while(i <= 0; i <= six.length; j++) //"Your journey continues through the following stops: 23rd, 28th, 33rd."
+ };else (six = endDest){
+    six = endDest.includes('33rd'); // "7 stops in total."
+ }
+ console.log("N", "Times Square", "6", "33rd");
