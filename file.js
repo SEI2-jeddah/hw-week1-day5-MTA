bbd3a4f7e8de9faf2f18582c0f921fbdaf174aa3
Ali-hd <ali.hamidaddin1@gmail.com>
2019-10-05 12:32:19 +0300

    New code! fixed all

--- file.js
+++ file.js
@@ -1,151 +1,155 @@
 function subway(atline,atstop,toline,tostop){
-  let sen = "";
-  let rtv = "";
+  let sen = [];
+  let rtv = [];
   let sameline = 0;
-
+  let x = "";
+  let y = "";
+  let countA = 0;
+  let countB = 0;
+  let countC = 0;
+  let sum = 0;
 let sub = {
     N : ["Times Square","34th","28th","23rd","Union Square","8th"],
     L : ["8th","6th","Union Square","3rd","1st"],
     6 : ["Grand Central","33rd","28th","23rd","Union Square","Astor Place"]
   }
-  for(let i = (sub[atline].indexOf(atstop))+1;i<=(sub[atline].length);i++){
-    
+
+  for(let i = sub[atline].indexOf(atstop)+1;i<sub[atline].length;i++){
+
     if(sub[atline][i]=="Union Square"){
-
-      sen = sen + sub[atline][i]
-      sen = sen + "."
-      
+      sen.push(sub[atline][i])
+      sen.push(".")
+      countA+=1
       break;
     }
-    
-    sen = sen + sub[atline][i]
-    sen = sen + ", "
-    
-    if(sub[atline][i]==tostop && atline==toline){
-      sameline = sen ;
+
+    if(atline == toline && sub[atline][i]==tostop){
+      sen.push(sub[atline][i])
+      sen.push(".")
+      countA+=1
       break;
     }
-    }
 
-   if(sen.indexOf("Union Square")==-1){
-     
-      sen=""
-   }
+      sen.push(sub[atline][i])
+      sen.push(", ")
+      countA+=1
 
-  if(sen==""){
-    
-    for(let i = ((sub[atline].indexOf(atstop))-1);i>=0;i--){
-      
+  }
+
+if(!(sen.includes("Union Square")) && atstop != "Union Square" && atline != toline){
+
+  sen = [];
+  countA=0;
+  for(let i = sub[atline].indexOf(atstop)-1;i>=0;i--){
+
     if(sub[atline][i]=="Union Square"){
-      sen = sen + sub[atline][i]
-      sen = sen + "."
-      
+      sen.push(sub[atline][i])
+      sen.push(".")
+      countA+=1
       break;
-    } 
-      
-     sen = sen + sub[atline][i]
-     sen = sen + ", " 
-     
+    }
+
+      sen.push(sub[atline][i])
+      sen.push(", ")
+      countA+=1
   }
 
+}
+
+if(atline == toline && sameline == 0){
+  sameline = sen
+  countB=countA;}
+if(atline == toline && !(sameline.includes(tostop))){
+  sameline = []
+  countB=0;
+  for(let i = sub[atline].indexOf(atstop)-1;i>=0;i--){
+    if(sub[atline][i]==tostop){
+
+      sameline.push(sub[atline][i])
+      sameline.push(".")
+      countB+=1
+      break;
+    }
+
+      sameline.push(sub[atline][i])
+      sameline.push(", ")
+      countB+=1
   }
-  
-  if(sen.indexOf('Union')>=0){ 
-    for(let i = sub[toline].indexOf("Union Square");i<sub[toline].length;i++){
-      
-      if(tostop == "Union Square"){
-        //console.log("You have reached your destination")
-        break;
-      }else
-      if(sub[toline][i]==tostop){
-        
-        rtv = rtv + sub[toline][i]
-        rtv = rtv + "."
-        break;
-      }
-        if(sub[toline][i]!="Union Square")
-        rtv = rtv + sub[toline][i]  
+}
+if(atline == toline && !(sameline.includes(tostop))){
+  sameline = []
+  countB=0;
+  for(let i = sub[atline].indexOf(atstop)+1;sub[atline].length;i++){
 
-        if(sub[toline][i]!="Union Square")
-        rtv = rtv + ", "
-        
-        
-      }
+    if(sub[atline][i]==tostop){
+      sameline.push(sub[atline][i])
+      sameline.push(".")
+      countB+=1
+      break;
+    }
+
+      sameline.push(sub[atline][i])
+      sameline.push(", ")
+      countB+=1
   }
-     
-      if(rtv.indexOf(tostop)==-1){
-        rtv = "";
-        for(let i = sub[toline].indexOf("Union Square");i>=0;i--){
-          
-          if(tostop == "Union Square"){
-              //console.log("You have reached your destination")
-            break;
-          }else
-        if(sub[toline][i]==tostop){   
-                rtv = rtv + sub[toline][i]
-                rtv = rtv + "."
-                break;
-        }
+}
 
-        if(sub[toline][i]!="Union Square")
-        rtv = rtv + sub[toline][i]  
+if(atline != toline){
+  for(let i = sub[toline].indexOf("Union Square")+1;i<=sub[atline].length;i++){
+    if(sub[toline][i]==tostop){
+      rtv.push(sub[toline][i])
+      rtv.push(".")
+      countC+=1
+      break;
+    }
+      rtv.push(sub[toline][i])
+      rtv.push(", ")
+      countC+=1
+  }
+}
+if(atline != toline && !(rtv.includes(tostop))){
+  rtv=[];
+  countC=0;
+  for(let i = sub[toline].indexOf("Union Square")-1;i>=0;i--){
 
-        if(sub[toline][i]!="Union Square")
-        rtv = rtv + ", "
+    if(sub[toline][i]==tostop){
+      rtv.push(sub[toline][i])
+      rtv.push(".")
+      countC+=1
+      break;
+    }
 
-        
-      }
-      }
+      rtv.push(sub[toline][i])
+      rtv.push(", ")
+      countC+=1
 
-     if(rtv.indexOf(tostop)==-1){
-        rtv = "";
-        for(let i = sub[toline].indexOf("Union Square");i>=0;i++){
-          
-          if(tostop == "Union Square"){
-              //console.log("You have reached your destination")
-            break;
-          }else
-        if(sub[toline][i]==tostop){   
-                rtv = rtv + sub[toline][i]
-                rtv = rtv + "."
-                break;
-        }
+  }
+}
 
-        if(sub[toline][i]!="Union Square")
-        rtv = rtv + sub[toline][i]  
+if(sameline.length>0){
+  x = sameline.join('')
+  countA=0
+}else{ x = sen.join('')
+countB=0}
+y = rtv.join('')
 
-        if(sub[toline][i]!="Union Square")
-        rtv = rtv + ", "
+if(atstop=="Union Square" && atline != toline){
+countA=0
+countB=0
+}else{console.log("You must travel through the following stops on the " + atline + " line: "+ x)}
 
-        
-      }
-      }
-
-  if(atstop == "Union Square")
-  sen = " "
-
-if(sameline>0 || sameline.length>0){sameline = sameline.slice(0,-2)};
-let add1 = sen.split(",")
-if(atline == toline && (sameline>0 || sameline.length>0)){add1 = sameline.split(",")}
-let add2 = rtv.split(",")
-if(atline == toline && (sameline>0 || sameline.length>0)){add2 = ""}
-let sum = (add1.length + add2.length)
-if(atline == toline && (sameline>0 || sameline.length>0)){sum += 1}
-if(atline == toline && (sameline>0 || sameline.length>0) && (sameline.indexOf('Union')==-1)||(atstop=="Union Square")){sum-=1}
-if(sameline.length > 0 && atstop!="Union Square"){
-  console.log("You must travel through the following stops on the " + atline + " line: "+sameline+ ".")
-}else if(atstop!="Union Square"){
-console.log("You must travel through the following stops on the " + atline + " line: "+sen)}
-if(atline!=toline){
+if(atline != toline && tostop != "Union Square"){
   console.log("Change at Union Square to line "+toline+".")
 }else if(tostop != "Union Square"){
   console.log("Stay on the same line")
 }
-if(tostop != "Union Square"){console.log("Your journey continues through the following stops: "+rtv)}
 
+if(tostop != "Union Square" && !(sameline>0 || sameline.length>0)){
+console.log("Your journey continues through the following stops: "+y)
+}else if(rtv.length>0 && tostop != "Union Square"){console.log("Your journey continues through the following stops: "+y)}else{countC=0}
 
+sum = countA + countB + countC;
 console.log( sum + " stops in total")
 }
 
-
-subway("L", "Union Square", "L", "6th")
+subway("6", "Union Square", "N", "34th");
