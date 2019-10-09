00b458264328f4739d50ee174c8802f3f4fb39d2
Doaa Alalawi <Doaa.alalawi@outlook.sa>
2019-10-09 22:53:11 +0300

    I solve it

--- hw-week1-day5-MTA.js
+++ hw-week1-day5-MTA.js
@@ -1,110 +1,63 @@
-function planTrip(Pline, Pspot, Wline, Wspot) {
+let Mta = {
+  N: ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"],
+  L: ["8th", "6th", "Union Square", "3rd", "1st"],
+  "6": ["Grand Central", "33rd", " 28th", "23rd", "Union Square", "Astor Place"]
+}
 
-    var L = ['8th','6th','Union Square','3rd','1st']
-    var N = ['TimesQuer','34th','28th','23rd','Union Square','8th']
-    var six =['Grand Central', '33rd', '28th', '23rd', 'Union Square', 'Astor Place']
-    
-    var steps = 0 
-    var move =''
-    var steps = 0
-    var Location
-    var goingTo
-    var usi
-    var sti
-    
-    // to get the index
-    if (Pline == "L") 
-    {
-      Location = L
-      usi = Location.indexOf(Pspot)
-    }
-    else if (Pline == "N") 
-    {
-       Location = N
-       usi = Location.indexOf(Pspot)
-    }
-    else if (Pline == "6") 
-    {
-       Location = six
-       usi = Location.indexOf(Pspot)
-    }
-    
-    if (Wline == "L") 
-    {
-      goingTo = L
-      sti = goingTo.indexOf(Wspot)
-    }
-    else if (Wline == "N") 
-    {
-      goingTo = N
-      sti = goingTo.indexOf(Wspot)
-    }
-    else if (Wline == "6") 
-    {
-      goingTo = six
-      sti = goingTo.indexOf(Wspot)
-    }
-    
-    // check if there are difrent line 
-    if (Pline == Wline)
-    {
-      // calculate the steps
-    if (usi >= 2 )
-    {
-      for (var i = 0; i < sti - usi; i++ )
-    {
-        steps ++
-    }
-    }
-    else if (usi == 1 )
-    {
-      for (var i = 1; i < sti; i++ )
-    {
-        steps ++
-    }
-    }
-    else 
-    {
-      for (var i = 0 ; i < sti ; i++ )
-    {
-        steps ++
-    } 
-    }
-    // get the spot
-    for (var i =0 ;i< steps;i++,usi++)
-    {
-      move += Location[usi+1]+"," 
-    }
-    console.log("You must travel through the following stops on the " + Pline+" line: " + move)
-    console.log(steps + " stops in total.")
-    }
-    else 
-    {
-      var countL = 0
-      var countG = 0
-      var L = Location.indexOf(Pspot)
-      var G = goingTo.indexOf(Wspot)
-    
-      for (var i = 0; i < Location.length ;i++)
-      {
-    
+function planTrip(stop1, Line1, stop2, Line2){
+
+  let line1 = Mta[Line1]
+  let line2 = Mta[Line2]
+  let trip1, trip2 = ""
+  let stops = 0
+
+  function oneLine(starting, end, line){
+      let trip= ''
+      let pos = line.indexOf(starting)
+  
+  if(line.indexOf(starting) < line.indexOf(end)){
+
+      stops +=  line.indexOf(end) - line.indexOf(starting) 
+      for(var i = pos+1; i <=line.indexOf(end); i++ ){
+          if(i == line.indexOf(end)){
+              trip += line[i] + '.'  
+          }else{
+          
+          trip += line[i] + ', '
+          }
       }
-       for (var i = G ; i < goingTo.length ;i++)
-      {
-        if (goingTo[G] === 'US')
-        {
-          countG++
-        }
+  }
+  else if(line.indexOf(starting) >line.indexOf(end)){
+      stops += line.indexOf(starting) - line.indexOf(end)
+      for(var i = pos-1; i > line.indexOf(end); i-- ){
+          if(i == line.indexOf(end)+1){
+              trip += line[i] + '.'  
+          }else{
+          
+          trip += line[i] + ', '
+          }
       }
-      console.log(countL)
-      console.log(countG)
-      // console.log("You must travel through the following stops on the " + Pline+" line: " + move)
-      // console.log("Change at Union Square.")
-      // console.log( "Your journey continues through the following stops: 23rd, 28th, 33rd.")
-      // console.log(steps + " stops in total.")
-    }
-    }
-    
-    planTrip("6", "Grand Central", "6", "23rd")
-    planTrip("6", "Grand Central", "N", "23rd")
-    
+  }
+  return trip 
+  }
+  
+  if(line1 == line2){
+      trip1 = oneLine(stop1, stop2, line2)
+      console.log(trip1)
+      console.log('trip')
+
+  }else if(line1 != line2){
+      trip1 = oneLine(stop1, 'Union Square', line1)
+
+      trip2 = oneLine('Union Square', stop2, line2)
+
+      console.log("You must travel through the following stops on the " + Line1 + " line: " + trip1)
+      console.log('Change at Union Square.')
+      console.log('Your journey continues through the following stops:' + trip2)
+      console.log(stops + " stops in total.");
+
+  }else{
+      console.log('you are there :P ')
+  }
+}
+planTrip("6th"  ,'L', '33rd', '6')
