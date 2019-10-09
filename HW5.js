0fb23ab4df92f97314272c27e36a026a76e09ccd
diala.ghamdi@gmail.com <you@example.com>
2019-10-09 21:48:45 +0300

    I solved it

--- HW5.js
+++ HW5.js
@@ -1,69 +1,57 @@
+// console.log() shows output similar to this:
+// "You must travel through the following stops on the N line: 34th, 28th, 23rd, Union Square."
+// "Change at Union Square."
+// "Your journey continues through the following stops: 23rd, 28th, 33rd."
+// "7 stops in total."
 
- /*There are 3 subway lines:
-The N line has the following stops: Times Square, 34th, 28th, 23rd, Union Square, and 8th
-The L line has the following stops: 8th, 6th, Union Square, 3rd, and 1st
-The 6 line has the following stops: Grand Central, 33rd, 28th, 23rd, Union Square, and Astor Place.
-All 3 subway lines intersect at Union Square, but there are no other intersection points. (For example, this means the 28th stop on the N line is different than the 28th street stop on the 6 line, so you'll have to differentiate this when you name your stops in the arrays.)
-Tell the user the number of stops AND the stops IN ORDER that they will pass through or change at.*/  
-   
-   
-   let nline=['Time square','34th', '28th',' 23rd', 'Union Square',' 8th']
-   let lline =['8th', '6th', 'Union Square', '3rd','1st']
-   let thesixline=['Grand Central', '33rd', '28th','23rd', 'Union Square', ' Astor Place.']
-   
-   
-   function MTA (sp,ss,ep,es){
-   
-       let thestep =0
-       let fPoint
-       let unionPoint
-       let nPoint
-       let nextP
-       let unp
-      
-   
-       console.log("Welcome u r in  "+ss+' '+"Station ")
-   
-   if (sp == ep) {
-       
-       nextP = sp.indexOf(ss)
-       nPoint = sp.indexOf(es)
-      thesameline(nextP, nPoint)
-   
+
+let MTA = {
+    N: ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"],
+    L: ["8th", "6th", "Union Square", "3rd", "1st"],
+    "6": ["Grand Central", "33rd", " 28th", "23rd", "Union Square", "Astor Place"]
    }
-   
-        if (sp!=ep){
-       fPoint = sp.indexOf(ss)
-       unionPoint =sp.indexOf("(Union Square)")
-       thesameline(fPoint,unionPoint,sp)
-       unp = ep.indexOf("Union Square")
-       console.log("U have to change in (Union Square)")
-       nPoint = ep.indexOf(es)
-       thesameline(unp,nPoint,ep)
-       }
-   
-   function thesameline(nextP, nPoint,L) {
-       for (i = 0; i <= L.indexOf('Union Square'); i++) {
-           if (nextP> nPoint) {
-               console.log("U have to move to " + L [nextP--])
-               nextP = L.indexOf(L [nextP--] )
-               thestep++
-           }
-             if (nextP < nPoint) {
-               console.log("U have to move to " + L[nextP++])
-               nextP = L.indexOf(L[nextP++])
-               thestep++
-           }
-         else if(nextP == nPoint) {
-         
-               console.log("U are in " + L[nextP])
-   
-             
-           }}}
-   
-  
-
-     console.log("total steps is " + thestep)
-}
- 
-   console.log("The total of ur steps count is " + MTA(nline, "Times Square", thesixline, "33rd"))
+   function planTrip(stop1, Line1, stop2, Line2) {
+    let line1 = MTA[Line1]
+    let line2 = MTA[Line2]
+    let trip1, trip2 = ""
+    let stops = 0
+    function oneLine(starting, end, line) {
+      let trip = ''
+      let pos = line.indexOf(starting)
+      if (line.indexOf(starting) < line.indexOf(end)) {
+        stops += line.indexOf(end) - line.indexOf(starting)
+        for (var i = pos + 1; i <= line.indexOf(end); i++) {
+          if (i == line.indexOf(end)) {
+            trip += line[i] + '.'
+          } else {
+            trip += line[i] + ', '
+          }
+        }
+      } else if (line.indexOf(starting) > line.indexOf(end)) {
+        stops += line.indexOf(starting) - line.indexOf(end)
+        for (var i = pos - 1; i > line.indexOf(end); i--) {
+          if (i == line.indexOf(end) + 1) {
+            trip += line[i] + '.'
+          } else {
+            trip += line[i] + ', '
+          }
+        }
+      }
+      return trip
+    }
+    if (line1 == line2) {
+      trip1 = oneLine(stop1, stop2, line2)
+      console.log(trip1)
+      console.log('trip')
+    } else if (line1 != line2) {
+      trip1 = oneLine(stop1, 'Union Square', line1)
+      trip2 = oneLine('Union Square', stop2, line2)
+      console.log("You must travel through the following stops on the " + Line1 + " line: " + trip1)
+      console.log('Change at Union Square.')
+      console.log('Your journey continues through the following stops : ' + trip2)
+      console.log(stops + " stops in total.");
+    } else {
+      console.log('you are there ')
+    }
+   }
+   planTrip("Times Square", 'N', '33rd', '6')
